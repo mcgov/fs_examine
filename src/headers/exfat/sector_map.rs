e60c96edfb985da22f2879adf174b54c4b24649a
Matthew G. McGovern <matthew@mcgov.dev>
2022-04-04 16:34:54 -0700

    add sector map for main and backup boot regions

---
+++ src/headers/exfat/sector_map.rs
@@ -0,0 +1,16 @@
+// Regions Name
+
+// Comments (sizes and offsets are in sectors)
+// Main Boot Region offset size
+// Main Boot Sector	0	1
+// Main Extended Boot Sectors	1	8
+// Main OEM Parameters	9	1
+// Main Reserved	10	1
+// Main Boot Checksum	11	1
+
+// Backup Boot Region
+// Backup Boot Sector	12	1
+// Backup Extended Boot Sectors	13	8
+// Backup OEM Parameters	21	1
+// Backup Reserved	22	1
+// Backup Boot Checksum	23	1
