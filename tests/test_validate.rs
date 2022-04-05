b67ae6eadc34b45e731a98bb92ac0c704092f671
Matthew G. McGovern <matthew@mcgov.dev>
2022-04-05 08:09:01 -0700

    fix tests to use offset read

--- tests/test_validate.rs
+++ tests/test_validate.rs
@@ -1,25 +1,25 @@
 use xfat::headers::exfat;
 use xfat::headers::mbr::*;
-use xfat::headers::reader::read_header_from_file;
+use xfat::headers::reader::read_header_from_offset;
 
 #[test]
 fn test_validate_known_boot_sector() {
     let processed_header =
-        read_header_from_file::<exfat::boot_sector::BootSector>("tests/main_boot_sector.bin", 0);
+        read_header_from_offset::<exfat::boot_sector::BootSector>("tests/main_boot_sector.bin", 0);
     assert_eq!(processed_header.validate_header(), true);
 }
 
 #[test]
 fn test_read_known_mbr() {
-    let processed_header = read_header_from_file::<Mbr>("tests/mbr.bin", 0);
+    let processed_header = read_header_from_offset::<Mbr>("tests/mbr.bin", 0);
     println!("{:?}", processed_header);
 }
 
 #[test]
 fn test_read_mbr_and_boot_sector() {
     let file = "tests/mbr_and_mbs.bin";
-    let mbr = read_header_from_file::<Mbr>(&file, 0);
-    let main_boot_sector = read_header_from_file::<exfat::boot_sector::BootSector>(
+    let mbr = read_header_from_offset::<Mbr>(&file, 0);
+    let main_boot_sector = read_header_from_offset::<exfat::boot_sector::BootSector>(
         &file,
         mbr.partitions[0].lba_of_partition_start as u64 * 512,
     );
